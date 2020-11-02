7580178c9ee0565337e1988b7730a79155644aef
Allie Towe <towe.allie@yahoo.com>
2020-11-02 14:15:37 -0600

    photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Room Porn';
+  image1 = 'https://i.pinimg.com/originals/91/ec/5a/91ec5ab3c523d42f33f2aa9e8035e2fb.jpg';
+  image2 = 'https://i.pinimg.com/564x/be/1f/2f/be1f2f59943ab642337cbc5f406877d2.jpg';
+  image3 = 'https://i.pinimg.com/originals/f7/40/77/f7407720ac3cc373a06a0b11e7915614.jpg';
 
   constructor() { }
 
