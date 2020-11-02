f14334aba74d119eeba6144c7a3b2435510220e8
Allie Towe <towe.allie@yahoo.com>
2020-11-02 16:07:43 -0600

    finished

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Room Porn';
+  photosTitle = 'Interior Design Porn';
   image1 = 'https://i.pinimg.com/originals/91/ec/5a/91ec5ab3c523d42f33f2aa9e8035e2fb.jpg';
   image2 = 'https://i.pinimg.com/564x/be/1f/2f/be1f2f59943ab642337cbc5f406877d2.jpg';
   image3 = 'https://i.pinimg.com/originals/f7/40/77/f7407720ac3cc373a06a0b11e7915614.jpg';
